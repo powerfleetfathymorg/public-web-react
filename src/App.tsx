04df650f9d78ac16c0d463543287085364a35dde
Jeremy Tomlinson <jeremy.tomlinson@gmail.com>
2022-09-22 15:13:28 -0600

    added emotibit dashboard and logos

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,17 @@
 import React from 'react';
-import logo from './powerfleet-logo-80.jpg';
-import frameworkImage from './react.jpg';
+import fathymemotibit from './fathym-emotibit.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <a href="https://powerfleet.fathym-it.com/"><img src={logo} className="pf-logo" alt="logo" /></a>
-        <div className="siteNav">
-          <a href="https://powerfleet.fathym-it.com/dash1/">
-            <button className="navbutton">Dash 1</button>
-          </a>
-          <a href="https://powerfleet.fathym-it.com/dash2/">
-            <button className="navbutton">Dash 2</button>
-          </a>
-          <a href="https://powerfleet.fathym-it.com/dash3/">
-            <button className="navbutton">Dash 3</button>
-          </a>
-        </div>
+        <a href="https://powerfleet.fathym-it.com/"><img src={fathymemotibit} className="fathymemotibit" alt="logo" /></a>        
       </header>
       <br />
       <div className="dashboard">
-        <iframe title="powerfleet-LV450-reefer-tracking - Page 1" className="responsive-iframe" src="https://app.powerbi.com/view?r=eyJrIjoiOGJjYTA0MzktMWY4MS00M2I4LTkzYTctMjRmNzQ1YjU1YjU5IiwidCI6IjZkY2JlYmQwLWY4ZDAtNGE5ZC04OWU1LTU4NzNlODE0NmIwYSIsImMiOjR9" frameBorder="0"></iframe>
+        <iframe title="Emotibit" className="responsive-iframe" src="https://app.powerbi.com/reportEmbed?reportId=bc2aeda7-6aeb-4bff-a232-c5ee354fd0b8&autoAuth=true&ctid=6dcbebd0-f8d0-4a9d-89e5-5873e8146b0a" frameBorder="0"></iframe>
       </div>
-      <br />
-      <div className="builtin">
-          Built in React<br />
-          <img src={frameworkImage} className="frameworkImage" alt="React" />
-        </div> 
     </div>
   );
 }
